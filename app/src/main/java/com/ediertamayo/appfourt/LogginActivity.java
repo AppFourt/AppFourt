3e23f72449c879cd7836f8b749e60d3288f635b5
ediertamayo <witali12>
2018-03-14 14:58:51 -0500

    Merge branches 'DesarolloJhon' and 'DesarrolloEdier' of https://github.com/ediertamayo/AppFourt into DesarrolloEdier
    # Conflicts:
    #	app/src/main/java/com/ediertamayo/appfourt/MainActivity.java

--- app/src/main/java/com/ediertamayo/appfourt/LogginActivity.java
+++ app/src/main/java/com/ediertamayo/appfourt/LogginActivity.java
@@ -1,13 +1,29 @@
 package com.ediertamayo.appfourt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LogginActivity extends AppCompatActivity {
-
+    EditText etUsuario,etClave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loggin);
+        etUsuario=findViewById(R.id.etUsuario);
+        etClave=findViewById(R.id.etClave);
+
+    }
+
+    public void registro(View view) {
+        Intent i=new Intent(this,RegistroActivity.class);
+        startActivityForResult(i,123);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/com/ediertamayo/appfourt/RegistroActivity.java
+++ app/src/main/java/com/ediertamayo/appfourt/RegistroActivity.java
@@ -1,13 +1,28 @@
 package com.ediertamayo.appfourt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class RegistroActivity extends AppCompatActivity {
-
+    Bundle extras=getIntent().getExtras();
+    EditText etUsuario,etClave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        etUsuario=findViewById(R.id.etUsuario);
+        etClave=findViewById(R.id.etClave);
+    }
+
+    public void guardar(View view) {
+        Intent intent=new Intent();
+        intent.putExtra("retUsuario",etUsuario.getText().toString());
+        intent.putExtra("retClave",etClave.getText().toString());
+        setResult(RESULT_OK,intent);
+        finish();
     }
 }
