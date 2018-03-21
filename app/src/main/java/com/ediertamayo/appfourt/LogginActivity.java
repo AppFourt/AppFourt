5d0b6854d7763e388c79584604a27bb21f33bee0
ediertamayo <witali12>
2018-03-21 02:26:16 -0500

    Merge branches 'DesarolloJhon' and 'DesarrolloEdier' of https://github.com/ediertamayo/AppFourt into DesarrolloEdier
    # Conflicts:
    #	app/src/main/java/com/ediertamayo/appfourt/MainActivity.java

--- app/src/main/java/com/ediertamayo/appfourt/LogginActivity.java
+++ app/src/main/java/com/ediertamayo/appfourt/LogginActivity.java
@@ -9,8 +9,8 @@ import android.widget.Toast;
 
 public class LogginActivity extends AppCompatActivity {
     EditText etUsuario,etClave;
-    String usuarioR;
-    String claveR;
+    String usuarioL;
+    String claveL,nombreL,emailL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,15 +21,24 @@ public class LogginActivity extends AppCompatActivity {
 
     public void registro(View view) {
         Intent intent=new Intent(LogginActivity.this,RegistroActivity.class);
-        startActivityForResult(intent,123);
+        startActivityForResult(intent,1);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode==123 && resultCode == RESULT_OK){
-            usuarioR=String.valueOf(data.getExtras().getString("retUsuario"));
-            claveR=String.valueOf(data.getExtras().getString("retClave"));
-        } else {
+        if (requestCode==1 && resultCode == RESULT_OK){
+            usuarioL=String.valueOf(data.getExtras().getString("usuarioR"));
+            claveL=String.valueOf(data.getExtras().getString("claveR"));
+            nombreL=String.valueOf(data.getExtras().getString("nombreR"));
+            emailL=String.valueOf(data.getExtras().getString("emailR"));
+        }else if(requestCode==2 && resultCode==RESULT_OK) {
+            usuarioL=String.valueOf(data.getExtras().getString("usuarioM"));
+            claveL=String.valueOf(data.getExtras().getString("claveM"));
+            nombreL=String.valueOf(data.getExtras().getString("nombreM"));
+            emailL=String.valueOf(data.getExtras().getString("emailM"));
+        }else if(requestCode==2 && resultCode==RESULT_CANCELED) {
+            finish();
+        } else{
             if (resultCode == RESULT_CANCELED){
                 Toast.makeText(LogginActivity.this, "Falló registro",
                         Toast.LENGTH_SHORT).show();
@@ -39,12 +48,15 @@ public class LogginActivity extends AppCompatActivity {
     }
 
     public void ingresar(View view) {
-        if(etUsuario.getText().toString().equals(usuarioR)&&etClave.getText().toString().equals(claveR)){
+        if(etUsuario.getText().toString().equals(usuarioL)&&etClave.getText().toString().equals(claveL)){
             Intent intent=new Intent(LogginActivity.this,MainActivity.class);
-            intent.putExtra("usuarioL",usuarioR);
-            intent.putExtra("claveL",claveR);
-            startActivity(intent);
-            finish();
+            intent.putExtra("usuarioL",usuarioL);
+            intent.putExtra("claveL",claveL);
+            intent.putExtra("nombreL",claveL);
+            intent.putExtra("emailL",claveL);
+            etUsuario.setText("");
+            etClave.setText("");
+            startActivityForResult(intent,2);
         }else{
             Toast.makeText(LogginActivity.this, "Datos incorrectos",
                     Toast.LENGTH_SHORT).show();
